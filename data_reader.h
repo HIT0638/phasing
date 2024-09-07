4872304b2b848313e6537c4cc30228c9b6266c5f
tengbo <156294753@qq.com>
2024-09-07 13:51:16 +0800

    ready

--- data_reader.h
+++ data_reader.h
@@ -114,7 +114,37 @@ public:
 };
 
 
+/**************
+ *    BAM     *
+ **************/
+struct Allele_Call {
+    /** 
+     * 一个等位基因（附加存在），根据后面的用途：
+     * SNPs会分别按reads和vcf文件进行存储，即记录各read上的SNPs信息 + 保存vcf文件中SNPs的信息
+     * 因此该数据结构作为一个附加的存在，需记录
+     * 1. 其在query read上的位置，也就是read上第几个snp
+     * 2. 在vcf文件SNPs上的位置，同样，也是基于index保存
+     * 3. 该等位基因信息，方便起见， 0表示与REF相同，1表示与read支持的ALT相同，-1表示gap/unknown
+    */
+   int que_pos; //position on query read
+   int snp_idx; //position on vcf snps
+   int allele;
+   Allele_Call(int q, int s, int a): que_pos(q), snp_idx(s), allele(a) {}
+};
+typedef std::vector<Allele_Call> Read_Allele; // 以read为单位保存SNP的形式
 
+/**
+ * Detect alleles by realignment
+ * 这个函数会进行realignment。
+ * @param bam_fn aligned reads
+ * @param chr_name chromosome name
+ * @param snps variants to phase
+ * @param len reference sequence length
+ * @param seq reference sequence
+ * @return all alleles on informative reads
+ */
+std::vector<Read_Allele> detect_allele(const char *bam_fn, const std::string &chr_name,
+									   std::vector<SNP> &snps, int len, const char *seq);
 
 #endif
 
