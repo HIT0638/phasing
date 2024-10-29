de8a5e76671748875ae41db17c198ec7c1c9f29e
tengbo <156294753@qq.com>
2024-10-29 15:18:16 +0800

    realign

--- data_reader.cpp
+++ data_reader.cpp
@@ -6,6 +6,7 @@
 
 #include "data_reader.h"
 #include "htslib/sam.h"
+#include "realignment.h"
 
 void VCF_Header::add_line(const char *buf) {
     if(buf[0] == '#' and buf[1] == '#'){
@@ -198,4 +199,125 @@ char * FASTA_Reader::get_contig(const std::string &chr_name) {
     delete[] buffer;
     assert(length == faidx.length);
     return ref;
+}
+
+static int binary_search_snp(const std::vector<SNP> &snps, int pos) {
+	int l = 0, r = snps.size() - 1, ret = -1;
+	while (l <= r) {
+		int mid = (l + r) / 2;
+		if (snps[mid].pos < pos) l = mid + 1;
+		else {
+			ret = mid;
+			r = mid - 1;
+		}
+	}
+	return ret;
+}
+
+std::vector<Read_Allele> detect_allele(const char *bam_fn, const std::string &chr_name,
+                                       std::vector<SNP> &snps, int len, const char *seq) {
+    std::vector<Read_Allele> ret; int total_allele = 0;
+    samFile *bam_fp = sam_open(bam_fn, "r");
+	if (bam_fn == nullptr) {
+		fprintf(stderr, "ERR: can not open BAM file %s\n", bam_fn);
+		std::abort();
+	}
+	bam_hdr_t *bam_header = sam_hdr_read(bam_fp);
+	if (bam_header == nullptr) {
+		fprintf(stderr, "ERR: can not read header in BAM file\n");
+		std::abort();
+	}
+	hts_idx_t *bam_idx = bam_index_load(bam_fn);
+	if (bam_idx == nullptr) {
+		fprintf(stderr, "ERR: can not load BAM index; use `samtools index`\n");
+		std::abort();
+	}
+	bam1_t *aln = bam_init1();
+	hts_itr_t *iter = sam_itr_querys(bam_idx, bam_header, chr_name.c_str());
+	if (iter == nullptr) {
+		fprintf(stderr,"ERR: invalid region for chromosome %s\n", chr_name.c_str());
+		std::abort();
+    }
+    Realignment realign(len, seq);
+    while(true) {
+        int got_any = sam_itr_next(bam_fp, iter, aln);
+        if (got_any < 0) break;
+
+        int ref_start = aln->core.pos + 1;
+        int bs = binary_search_snp(snps, ref_start);
+        if (bs == -1) continue;
+
+        Read_Allele real;
+        int read_len = 0, ref_len = 0;
+        const uint32_t *cigar_array = bam_get_cigar(aln);
+        for (int i = 0; i < aln->core.n_cigar; i ++) {
+            char op_chr = bam_cigar_opchr(cigar_array[i]);
+			int op_len = bam_cigar_oplen(cigar_array[i]);
+			if (op_chr != 'D' && op_chr != 'H') read_len += op_len;
+			if (op_chr != 'I' && op_chr != 'S' && op_chr != 'H') ref_len += op_len;
+        }
+
+        int cid = 0; // Cigar iterator
+		int que_pointer = 0, ref_pointer = ref_start; // Pointers sliding the aligned window
+		for (int i = bs; i < snps.size(); i++) {
+			auto &snp = snps[i];
+			if (snp.pos >= ref_start + ref_len) break;
+
+			// Find the cigar interval overlapping the SNP.
+			// The cigar intervals are consecutive.
+			// For example, 12D2X19=3I has cigar intervals [0,12), [12, 14), [14, 33), [33, 33).
+			while (cid < aln->core.n_cigar) {
+				char op_chr = bam_cigar_opchr(cigar_array[cid]);
+				int op_len = bam_cigar_oplen(cigar_array[cid]);
+				if (op_chr == 'H') { cid++; continue; }
+				if (op_chr == 'I' || op_chr == 'S') {
+					que_pointer += op_len;
+					cid++;
+					continue;
+				}
+				if (ref_pointer + op_len > snp.pos) break; // Cigar interval overlapping the SNP found
+				ref_pointer += op_len;
+				if (op_chr != 'D') que_pointer += op_len;
+				cid++;
+			}
+
+			char op_chr = bam_cigar_opchr(cigar_array[cid]);
+			int que_pos = op_chr == 'D' ?que_pointer - 1 : que_pointer + snp.pos - ref_pointer;
+			auto pair = realign.bit_vector_dp(aln, que_pos, snp.pos - 1, snp.alt);
+			int allele;
+			if (pair.first < pair.second) allele = 0;
+			else if (pair.first > pair.second) allele = 1;
+			else allele = -1;
+			real.emplace_back(Allele_Call(que_pos, i, allele));
+
+            // Remove marginal gaps
+		int l_active = -1, r_active = -2;
+		for (int i = 0; i < real.size(); i++) {
+			const auto &v = real[i];
+			if (v.allele == -1) continue;
+			if (l_active == -1) l_active = i;
+			r_active = i;
+		}
+		if (l_active == -1) continue;
+		Read_Allele no_gap_real;
+		for (int i = l_active; i <= r_active; i++) no_gap_real.push_back(real[i]);
+		real = no_gap_real;
+
+		// Only push back informative reads
+		if (real.size() >= 2) {
+			total_allele += real.size();
+			for (const auto &v : real) snps[v.snp_idx].add_read(ret.size(), v.allele);
+			ret.push_back(real);
+		}
+        }
+
+    }
+
+    bam_destroy1(aln);
+	bam_hdr_destroy(bam_header);
+	sam_close(bam_fp);
+	fprintf(stderr, "Detected %d alleles on %ld informative reads\n", total_allele, ret.size());
+	fprintf(stderr, "    Decompress BAM costs x CPU and x real seconds\n");
+	fprintf(stderr, "    Realignment costs x CPU and x real seconds\n");
+	return ret;
 }
--- main.cpp
+++ main.cpp
@@ -1,8 +1,63 @@
 #include <getopt.h>
 #include <cassert>
 
+#include "ksnp_reader.h"
 #include "realignment.h"
 
-int main(int argc, char** argv[]) {
+static int usage() {
+	fprintf(stderr, "Usage: phase -b <BAM> -r <FASTA> -v <VCF> -o <Output>\n");
+	fprintf(stderr, "  -b aligned reads in BAM format (indexed required)\n");
+	fprintf(stderr, "  -r reference sequence for allele realignment in FASTA format (indexed required)\n");
+	fprintf(stderr, "  -v heterozygous variants to phase in VCF format\n");
+	fprintf(stderr, "  -o output file that phased results are written to (stdout)\n");
+	fprintf(stderr, "  -c specify a chromosome to phase\n");
+	return 1;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc == 1) return usage();    
+
+    const char *bam_fn = nullptr, *vcf_fn = nullptr,  *ref_fn = nullptr, *output_fn = nullptr;
+	const char *request_chromosome = nullptr;
+	const char *resolution_fn = nullptr;
+    int c;
+    while ((c = getopt(argc, argv, "b:v:o:c:r:l:R:")) >= 0) {
+		if (c == 'b') {
+			bam_fn = optarg;
+		} else if (c == 'v') {
+			vcf_fn = optarg;
+		} else if (c == 'o') {
+			output_fn = optarg;
+		} else if (c == 'c') {
+			request_chromosome = optarg;
+		} else if (c == 'r') {
+			ref_fn = optarg;
+		} else return usage();
+	}
+
+    if (bam_fn == nullptr) { fprintf(stderr, "ERR: please assign a BAM file of aligned reads\n"); return 1; }
+	if (vcf_fn == nullptr) { fprintf(stderr, "ERR: please choose a VCF file to phase\n"); return 1; }
+	if (ref_fn == nullptr) {
+		fprintf(stderr, "ERR: please provide a reference sequence for detecting alleles by realignment\n" );
+		return 1;
+	}
+
+    auto variant_table = input_vcf(vcf_fn, request_chromosome);
+
+    FASTA_Reader ref_reader(ref_fn);
     
+    // enumerate chrs
+    for (int i = 0; i < variant_table.size; i ++) {
+        const auto &chr_name = variant_table.chromosome[i];
+		auto &snp_column = variant_table.variants[i]; // 对应染色体的所有snp
+		fprintf(stderr, "Phase %ld SNPs on chromosome %s\n", snp_column.size(), chr_name.c_str());
+
+		// Detecting alleles
+        int length = ref_reader.get_length(chr_name); assert(length > 0);
+        char *sequence = ref_reader.get_contig(chr_name);
+        std::vector<Read_Allele> read_row;
+        read_row = detect_allele(bam_fn, chr_name, snp_column, length, sequence);
+
+        delete [] sequence;
+    }
 }
